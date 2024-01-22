79517feed2377a3af960e51d0e02326490fa71b9
Lucas Alves Costa <109704795+lucas-costa-gb-tech@users.noreply.github.com>
2024-01-22 15:07:01 -0300

    feat: add a custom JavaScript action

---
+++ .github/actions/deploy-s3/index.js
@@ -0,0 +1,5 @@
+const core = require('@actions/core');
+// const github = require('@actions/github');
+// const exec = require('@actions/exec');
+
+core.notice('Custom action feita com JavaScript e Node');
