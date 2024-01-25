ba525d42b874adb4e70c533671da88e952fa4d67
Lucas Alves Costa <109704795+lucas-costa-gb-tech@users.noreply.github.com>
2024-01-25 17:48:13 -0300

    ci: remove unused packages

--- .github/actions/deploy-s3/main.js
+++ .github/actions/deploy-s3/main.js
@@ -1,6 +1,4 @@
 const core = require('@actions/core');
-// const github = require('@actions/github');
-// const exec = require('@actions/exec');
 
 const bucketName = core.getInput('bucket-name');
 const bucketRegion = core.getInput('bucket-region');
