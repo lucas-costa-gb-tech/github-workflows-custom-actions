9f3ebcc45c177c04f9e21854d6ff4297c6ffef0a
Lucas Alves Costa <109704795+lucas-costa-gb-tech@users.noreply.github.com>
2024-01-25 10:44:52 -0300

    ci: add inputs in the .js file

--- .github/actions/deploy-s3/main.js
+++ .github/actions/deploy-s3/main.js
@@ -2,4 +2,10 @@ const core = require('@actions/core');
 // const github = require('@actions/github');
 // const exec = require('@actions/exec');
 
-core.notice('Custom action feita com JavaScript e Node');
+const bucketName = core.getInput('bucket-name');
+const bucketRegion = core.getInput('bucket-region');
+const distFolder = core.getInput('dist-folder');
+
+core.notice(`O nome do bucket é ${bucketName}`);
+core.notice(`A região do bucket é ${bucketRegion}`);
+core.notice(`A pasta que deve ser usada para deploy é ${distFolder}`);
